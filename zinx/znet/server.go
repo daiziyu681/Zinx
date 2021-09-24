240294ec35a5de648fc929768a5b02180c695cb4
daiziyu681 <852956247@qq.com>
2021-09-24 14:56:22 +0800

    add output to server

--- zinx/znet/server.go
+++ zinx/znet/server.go
@@ -55,6 +55,7 @@ func (s* Server) Start() {
 						fmt.Println("recv buf error : ", err)
 						continue
 					}
+                    fmt.Printf("recv client msg %s, cnt : %d\n", buf, cnt)
 					if _, err := conn.Write(buf[:cnt]); err != nil {
 						fmt.Println("write back buf error : ", err)
 						continue
