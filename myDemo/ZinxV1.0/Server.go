55f2b029dedd70ddd26cacdd5938da6a772d09ce
daiziyu681 <852956247@qq.com>
2021-09-26 02:39:24 +0800

    self define test router and addrouter to test server

--- myDemo/ZinxV1.0/Server.go
+++ myDemo/ZinxV1.0/Server.go
@@ -1,8 +1,42 @@
 package main
 
-import "zinx/znet"
+import(
+	"fmt"
+	"zinx/znet"
+	"zinx/ziface"
+)
+
+// ping test self define router
+type PingRouter struct {
+	znet.BaseRouter
+}
+
+func (this *PingRouter) PreHandle(request ziface.IRequest) {
+	fmt.Println("call router PreHandle..")
+	_, err := request.GetConnection().GetTCPConnection().Write([]byte("before ping.."))
+	if err != nil {
+		fmt.Println("call back before ping error")
+	}
+}
+
+func (this *PingRouter) Handle(request ziface.IRequest) {
+	fmt.Println("call router Handle..")
+	_, err := request.GetConnection().GetTCPConnection().Write([]byte("ping...ping...ping..."))
+	if err != nil {
+		fmt.Println("call back ping ping error")
+	}
+}
+
+func (this *PingRouter) PostHandle(request ziface.IRequest) {
+	fmt.Println("call router PostHandle..")
+	_, err := request.GetConnection().GetTCPConnection().Write([]byte("after ping.."))
+	if err != nil {
+		fmt.Println("call back after ping error")
+	}
+}
 
 func main() {
 	s := znet.NewServer("[zinx V1.0]")
+	s.AddRouter(&PingRouter{})
 	s.Serve()
 }
